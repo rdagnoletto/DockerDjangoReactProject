0675d2cc30a2fe51f1ac278ac7d4eecc4c8c0d47
Rob Agnoletto <robagnoletto@Robs-MacBook-Pro.local>
2021-02-26 10:06:27 -0500

    Added auth and prediction api

--- backend/django_app/mainapp/settings.py
+++ backend/django_app/mainapp/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+
     'prediction',
+    'users',
 ]
 
 MIDDLEWARE = [
